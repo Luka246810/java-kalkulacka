38ded74a8ab60a945ea6fcc724e872fdfd84696e
Luka246810 <luka24681012@gmail.com>
2025-06-08 21:56:54 +0200

    Přidána metoda divide a zavolání v main, update pom.xml a další

---
+++ src/main/java/cz/itnetwork/Main.java
@@ -0,0 +1,43 @@
+package cz.itnetwork;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Zadejte číslo A: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Zadejte číslo B: ");
+        double b = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Součet: " + add(a, b));
+        System.out.println("Rozdíl: " + subtract(a, b));
+        System.out.println("Součin: " + multiply(a, b));
+        System.out.println("Podíl: " + divide(a, b));
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+     public static double multiply(double a, double b) {
+        return a * b;
+     }
+
+
+     public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Nelze dělit nulou");
+        }
+        return a / b;
+     }
+}
